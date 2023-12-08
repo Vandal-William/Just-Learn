fd8e3c5d9feee5895d21425d71a5418e7fe4997c
Vandal-William <vandal.william@hotmail.com>
2023-12-08 13:57:04 +0100

    fix deploy #11

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    // On utilise le polling pour que le hot reload fonctionne avec docker
-    watch: {
-      usePolling: true
-    },
-  }
+  base: '/Just-Learn/'
 })
